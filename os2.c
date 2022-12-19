276626b0cbc1865c444a66da234707224b273d6f
atrenes <84473636+atrenes@users.noreply.github.com>
2022-12-19 12:34:31 +0300

    create ppp_channel and output

--- os2.c
+++ os2.c
@@ -11,6 +11,7 @@
 #include <linux/mm.h>
 #include <linux/mm_types.h>
 #include <linux/of.h>
+#include <linux/ppp_channel.h>
 #include <linux/sched.h>
 #include <linux/types.h>
 
@@ -24,8 +25,10 @@ MODULE_VERSION("1.0");
 static struct dentry *module_dir;
 static struct dentry *module_file;
 static struct pci_dev *pci_device = NULL;
+static struct ppp_channel *ppp_chan = NULL;
 static void print_device(struct seq_file *file, struct pci_dev *pci_device);
 static int print_struct(struct seq_file *file, void *data);
+static void print_chan(struct seq_file *file, struct ppp_channel *ppp_chan);
 
 static ssize_t write_structures(struct file *file, const char __user *buffer, size_t length, loff_t *ptr_offset) {
 	printk(KERN_INFO "get arguments\n");
@@ -34,12 +37,20 @@ static ssize_t write_structures(struct file *file, const char __user *buffer, si
 	copy_from_user(user_data, buffer, length);
 	sscanf(user_data, "vendor_id: %x, device_id: %x", &vendor_id, &device_id);
 	pci_device = pci_get_device(vendor_id, device_id, NULL);
+	
+	if (ppp_register_channel(ppp_chan) == 0) {
+		printk(KERN_INFO "ppp_chan created");
+	} else {
+		printk(KERN_INFO "ppp_chan not created");
+	}
+	
 	single_open(file, print_struct, NULL);
 	return strlen(user_data);
 }
 
 static int print_struct(struct seq_file *file, void *data) {
     print_device(file, pci_device);
+    print_chan(file, ppp_chan);
     return 0;
 }
 
@@ -51,11 +62,25 @@ static void print_device(struct seq_file *file, struct pci_dev *pci_device) {
         seq_printf(file, "  vendor_ID: %u,\n", pci_device->vendor);
         seq_printf(file, "  device_ID: %u,\n", pci_device->device);
         seq_printf(file, "  init name: %s,\n", pci_device->dev.init_name);
-        //seq_printf(file, "  class name: %s,\n", pci_device->dev.class->name);
         seq_printf(file, "}\n");
     }
 }
 
+static void print_chan(struct seq_file *file, struct ppp_channel *ppp_chan) {
+	if (ppp_chan == NULL) {
+		seq_printf(file, "ppp_channel* is NULL");
+	} else {
+		seq_printf(file, "ppp_channel struct : {\n");
+		seq_printf(file, "    speed: %d, \n", ppp_chan->speed);
+		seq_printf(file, "    latency: %d, \n", ppp_chan->latency);
+		char *ppp_name = ppp_dev_name(ppp_chan);
+		if (ppp_name == NULL) {
+			seq_printf(file, "    ppp_name is NULL \n");
+		} else {
+			seq_printf(file, "    ppp_name: %s \n", ppp_name);
+		}
+	}
+}
 
 static struct file_operations fops = {
 	.read = seq_read,
