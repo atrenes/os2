5c11fb66935011624cd27e2c0209b7f6176c61fa
atrenes <hanalainendm@gmail.com>
2022-12-19 05:06:26 +0300

    i have no idea

--- os2.c
+++ os2.c
@@ -1,3 +1,4 @@
+#include <linux/device/class.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -9,27 +10,72 @@
 #include <linux/pid.h>
 #include <linux/mm.h>
 #include <linux/mm_types.h>
+#include <linux/of.h>
 #include <linux/sched.h>
 #include <linux/types.h>
 
-MODULE_LICENSE("GPL");
+#define BUFFER_SIZE 128
+
+MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("atrenes");
 MODULE_DESCRIPTION("os lab2 module for debugfs interaction");
 MODULE_VERSION("1.0");
 
 static struct dentry *module_dir;
 static struct dentry *module_file;
-static struct device *device;
+static struct pci_dev *pci_device = NULL;
+static void print_device(struct seq_file *file, struct pci_dev *pci_device);
+static int print_struct(struct seq_file *file, void *data);
+
+static ssize_t write_structures(struct file *file, const char __user *buffer, size_t length, loff_t *ptr_offset) {
+	printk(KERN_INFO "get arguments\n");
+	char user_data[BUFFER_SIZE];
+	int pid;
+	unsigned int vendor_id, device_id;
+	copy_from_user(user_data, buffer, length);
+	sscanf(user_data, "vendor_id: %x, device_id: %x", &vendor_id, &device_id);
+	pci_device = pci_get_device(vendor_id, device_id, NULL);
+	single_open(file, print_struct, NULL);
+	return strlen(user_data);
+}
+
+static int print_struct(struct seq_file *file, void *data) {
+    print_device(file, pci_device);
+    return 0;
+}
+
+static void print_device(struct seq_file *file, struct pci_dev *pci_device) {
+    if (pci_device == NULL) {
+        seq_printf(file, "Can't find pci_device with this params\n");
+    } else {
+        seq_printf(file, "pci_device structure: {\n");
+        seq_printf(file, "  vendor_ID: %u,\n", pci_device->vendor);
+        seq_printf(file, "  device_ID: %u,\n", pci_device->device);
+        seq_printf(file, "  init name: %s%,\n", pci_device->dev.init_name);
+        seq_printf(file, "  class name: %s,\n", pci_device->dev.class->name);
+        seq_printf(file, "}\n");
+    }
+}
+
 
 static struct file_operations fops = {
 	.read = seq_read,
+	.write = write_structures
 };
 
 static int __init mod_init(void) {
 	printk(KERN_INFO "os2 module loaded\n");
 	module_dir = debugfs_create_dir("os2", NULL);
 	module_file = debugfs_create_file("file", 666, module_dir, NULL, &fops);
-	dev_t majmin = MKDEV(8, 0); // major and minor numbers 8 and 0 respectively
+	
+	//dev_t _devt = MKDEV(13, 32); // major and minor numbers 13 and 32 respectively (mouse0?)
+	/*
+	node = of_node_get(NULL);
+	node = of_root;
+	printk(KERN_INFO "%s", node->name);
+	
+	pochemy ono ne rabotaet ya tak bolshe ne mogy
+	*/
 	
 	return 0;
 }
--- user.cpp
+++ user.cpp
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
-int main() {
+#define BUFFER_SIZE 128
+int main(int argc, char *argv[]) {
     FILE *file = fopen("/sys/kernel/debug/os2/file", "r+");
     if (file == NULL) {
 	    std::cout << "can't open file\n";
@@ -13,6 +13,27 @@ int main() {
 
     std::cout << "opened file\n";
 
+	if (strcmp(argv[1],"--help")==0) {
+        printf("first arg: vendor_id\n");
+        printf("second arg: device_id\n");
+        fclose(file);
+        return 0;
+    }
+
+	unsigned int vendor_id;
+    unsigned int device_id;
+	if (sscanf(argv[1], "%x", &vendor_id) && sscanf(argv[2], "%x", &device_id)) {
+        char buffer[BUFFER_SIZE];
+        fprintf(file, "vendor_id: %x, device_id: %x", vendor_id, device_id);
+        while (true) {
+            char *result = fgets(buffer, BUFFER_SIZE, file);
+            if (feof(file)) break;
+            printf(result);
+        }
+    } else {
+        printf("Аргументов много!!!");
+    }
+    
     fclose(file);
     return 0;
 }
