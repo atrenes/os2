a24168859bbf471f4e834e2c4ff5e65336948226
atrenes <hanalainendm@gmail.com>
2022-12-19 10:39:35 +0300

    IT WORKS

--- os2.c
+++ os2.c
@@ -30,7 +30,6 @@ static int print_struct(struct seq_file *file, void *data);
 static ssize_t write_structures(struct file *file, const char __user *buffer, size_t length, loff_t *ptr_offset) {
 	printk(KERN_INFO "get arguments\n");
 	char user_data[BUFFER_SIZE];
-	int pid;
 	unsigned int vendor_id, device_id;
 	copy_from_user(user_data, buffer, length);
 	sscanf(user_data, "vendor_id: %x, device_id: %x", &vendor_id, &device_id);
@@ -51,8 +50,8 @@ static void print_device(struct seq_file *file, struct pci_dev *pci_device) {
         seq_printf(file, "pci_device structure: {\n");
         seq_printf(file, "  vendor_ID: %u,\n", pci_device->vendor);
         seq_printf(file, "  device_ID: %u,\n", pci_device->device);
-        seq_printf(file, "  init name: %s%,\n", pci_device->dev.init_name);
-        seq_printf(file, "  class name: %s,\n", pci_device->dev.class->name);
+        seq_printf(file, "  init name: %s,\n", pci_device->dev.init_name);
+        //seq_printf(file, "  class name: %s,\n", pci_device->dev.class->name);
         seq_printf(file, "}\n");
     }
 }
@@ -66,7 +65,7 @@ static struct file_operations fops = {
 static int __init mod_init(void) {
 	printk(KERN_INFO "os2 module loaded\n");
 	module_dir = debugfs_create_dir("os2", NULL);
-	module_file = debugfs_create_file("file", 666, module_dir, NULL, &fops);
+	module_file = debugfs_create_file("file", 0666, module_dir, NULL, &fops);
 	
 	//dev_t _devt = MKDEV(13, 32); // major and minor numbers 13 and 32 respectively (mouse0?)
 	/*
