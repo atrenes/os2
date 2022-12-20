3c073389c4151af93d874f19eb63d3066aa590d5
atrenes <84473636+atrenes@users.noreply.github.com>
2022-12-20 10:24:21 +0300

    beautify user.cpp

--- user.cpp
+++ user.cpp
@@ -7,22 +7,22 @@
 int main(int argc, char *argv[]) {
     FILE *file = fopen("/sys/kernel/debug/os2/file", "r+");
     if (file == NULL) {
-	    std::cout << "can't open file\n";
-	    return 1;
+        std::cout << "can't open file\n";
+	return 1;
     }
 
     std::cout << "opened file\n";
 
-	if (strcmp(argv[1],"--help")==0) {
+    if (strcmp(argv[1],"-help")==0) {
         printf("first arg: vendor_id\n");
         printf("second arg: device_id\n");
         fclose(file);
         return 0;
     }
 
-	unsigned int vendor_id;
+    unsigned int vendor_id;
     unsigned int device_id;
-	if (sscanf(argv[1], "%x", &vendor_id) && sscanf(argv[2], "%x", &device_id)) {
+    if (sscanf(argv[1], "%x", &vendor_id) && sscanf(argv[2], "%x", &device_id)) {
         char buffer[BUFFER_SIZE];
         fprintf(file, "vendor_id: %x, device_id: %x", vendor_id, device_id);
         while (true) {
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
             printf(result);
         }
     } else {
-        printf("Аргументов много!!!");
+        printf("too many args");
     }
     
     fclose(file);
